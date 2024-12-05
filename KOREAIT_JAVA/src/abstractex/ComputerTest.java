70f38a37501223144908b658bf764a1e1ae0de2f
laborlaw93 <laborlaw93@gmail.com>
2024-12-05 13:35:14 +0900

    DOit

---
+++ KOREAIT_JAVA/src/abstractex/ComputerTest.java
@@ -0,0 +1,13 @@
+package abstractex;
+
+public class ComputerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+////		Computer c1 = new Computer();
+//		Computer c2 = new DeskTop();
+////		Computer c3 = new NoteBook(); 추상은 사용불가
+//		Computer c4 = new MyNoteBook();
+	}
+
+}
